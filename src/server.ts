01b243bd933fc4b06ccc565192f130b55d391885
Pierre Rodrigues <pierredcrodri@gmail.com>
2025-02-09 11:13:34 +0000

    Render the 404 page

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ server.use(express.static(path.join(__dirname, "../public")));
 
 server.use(mainRoutes);
 server.use((req, res) => {
-  res.status(403).send("Página não encontrada.");
+  res.status(404).render("pages/404")
 });
 
 server.listen(process.env.PORT, () => {
